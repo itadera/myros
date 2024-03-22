cebd0e64cbd65b345c13f9642c135f26d2c9e257
Shunki Itadera <s.itadera@aist.go.jp>
2024-03-23 02:46:56 +0900

    gix ros2

--- docker/ros2/run.sh
+++ docker/ros2/run.sh
@@ -1,4 +1,4 @@
 IMAGE_NAME=myhumble
 CONTAINER_NAME=ros2
 
-rocker --nvidia --x11 --network host --home --user --name $CONTAINER_NAME $IMAGE_NAME:latest $@
+rocker --nvidia --x11 --env CONTAINER_NAME=$CONTAINER_NAME --network host --home --user --name $CONTAINER_NAME $IMAGE_NAME:latest $@
