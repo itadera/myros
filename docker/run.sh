4d99eb1cdd73c333c3a5e72196d4d44a7a52606c
Shunki Itadera <s.itadera@aist.go.jp>
2024-04-03 14:41:37 +0900

    change root image for using on ARM

--- docker/run.sh
+++ docker/run.sh
@@ -11,4 +11,11 @@ else
   return
 fi
 
-rocker --nvidia --x11 --env CONTAINER_NAME=$CONTAINER_NAME --network host --home --user --name $CONTAINER_NAME $IMAGE_NAME:latest $2
+
+shopt -s nocasematch
+NVIDIA_FLAG=""
+if [[ $(lspci | grep -i '.* vga .* nvidia .*') == *' nvidia '* ]]; then
+  NVIDIA_FLAG="--nvidia"
+fi
+
+rocker $NVIDIA_FLAG --x11 --env CONTAINER_NAME=$CONTAINER_NAME --network host --home --user --name $CONTAINER_NAME $IMAGE_NAME:latest $2
