ba93167f77bbd7591cfb6015ba71f65c67f11e8e
Shunki Itadera <s.itadera@aist.go.jp>
2024-04-10 17:30:40 +0900

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,10 @@
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
 
-echo "" >> ~/.zshrc
-echo "# setup for ROS" >> ~/.zshrc
-echo "source $SCRIPT_DIR/ros_setup.zsh" >> ~/.zshrc
+if ! grep -q "source $SCRIPT_DIR/ros_setup.zsh" ~/.zshrc ; then
+  echo "" >> ~/.zshrc
+  echo "# setup for ROS" >> ~/.zshrc
+  echo "source $SCRIPT_DIR/ros_setup.zsh" >> ~/.zshrc
+fi
 
 # cp $SCRIPT_DIR/p10k.zsh ~/.p10k.zsh
-ln -s $SCRIPT_DIR/p10k.zsh ~/.p10k.zsh
+ln -s -f $SCRIPT_DIR/p10k.zsh ~/.p10k.zsh
